90fb0a60832ce0dc71bf72fe2f7568f707f9f268
RA4Z <rraz639@gmail.com>
2023-07-11 12:56:52 -0300

    Criando um método abstrato

--- Conta.py
+++ Conta.py
@@ -1,10 +1,16 @@
-class Conta:
+from abc import ABCMeta, abstractmethod
+
+class Conta(metaclass=ABCMeta):
     def __init__(self, codigo):
         self._codigo = codigo
         self._saldo = 0
 
     def deposita(self, valor):
         self._saldo += valor
+
+    @abstractmethod
+    def passa_o_mes(self):
+        pass
 
     def __str__(self):
         return '[>>Codigo {} Saldo {}<<]'.format(self._codigo, self._saldo)
@@ -18,12 +24,18 @@ class ContaPoupanca(Conta):
         self._saldo *= 1.01
         self._saldo -= 3
 
+class ContaInvestimento(Conta):
+    def passa_o_mes(self):
+        pass
+
 conta16 = ContaCorrente(16)
 conta16.deposita(1000)
 conta17 = ContaPoupanca(17)
 conta17.deposita(1000)
+conta18 = ContaInvestimento(18)
+conta18.deposita(500)
 
-contas = [conta16, conta17]
+contas = [conta16, conta17, conta18]
 for conta in contas:
     conta.passa_o_mes()
     print(conta)
