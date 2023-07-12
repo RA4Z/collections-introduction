b248ccd5e77756eef642919d6e722e378d2c9880
RA4Z <rraz639@gmail.com>
2023-07-12 14:44:39 -0300

    Adicionando operações de conjuntos

--- Conjuntos.py
+++ Conjuntos.py
@@ -2,5 +2,11 @@ usuarios_data_science = {15, 23, 43, 56}
 usuarios_machine_learning = {13, 23, 56, 42}
 
 assistiram = usuarios_data_science | usuarios_machine_learning
+assistiramOsDois = usuarios_data_science & usuarios_machine_learning
+assistiramSomenteDataScience = usuarios_data_science - usuarios_machine_learning
+assistiuSomenteUm = usuarios_data_science ^ usuarios_machine_learning
 
-print(assistiram)
+print(f'Alunos que fizeram qualquer curso: {assistiram}')
+print(f'Alunos que fizeram os dois cursos: {assistiramOsDois}')
+print(f'Alunos que fizeram somente Data Science: {assistiramSomenteDataScience}')
+print(f'Alunos que fizeram somente um curso: {assistiuSomenteUm}')
