ba9268ccabbc9c3720a87847341420c92fa205ee
RA4Z <rraz639@gmail.com>
2023-07-12 10:54:08 -0300

    Implementação de __lt__

--- OrdenacaoContas.py
+++ OrdenacaoContas.py
@@ -9,6 +9,9 @@ class ContaSalario:
     def __str__(self):
         return '[>>Codigo {} Saldo {}<<]'.format(self._codigo, self._saldo)
     
+    def __lt__(self, outro):
+        return self._saldo < outro._saldo
+
     def __eq__(self, outro):
         if type(outro) != ContaSalario:
             return False
@@ -23,4 +26,9 @@ conta_karol.deposita(1000)
 contas = [conta_raz, conta_karol]
 
 for conta in contas:
+    print(conta)
+
+print(conta_karol > conta_raz)
+
+for conta in sorted(contas, reverse=True):
     print(conta)
