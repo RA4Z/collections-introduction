4b5a9fe944a32baecc9c138006e392f144479fdf
RA4Z <rraz639@gmail.com>
2023-07-12 14:51:17 -0300

    Picotando uma string e inserindo a mesma em um conjunto

--- Conjuntos.py
+++ Conjuntos.py
@@ -10,3 +10,13 @@ print(f'Alunos que fizeram qualquer curso: {assistiram}')
 print(f'Alunos que fizeram os dois cursos: {assistiramOsDois}')
 print(f'Alunos que fizeram somente Data Science: {assistiramSomenteDataScience}')
 print(f'Alunos que fizeram somente um curso: {assistiuSomenteUm}')
+
+usuarios = {1,5,76,34,52,13,17}     #CRIAÇÃO DE CONJUNTO
+usuarios.add(765)   #ADICIONANDO UM NÚMERO A UM CONJUNTO
+
+usuarios = frozenset(usuarios)  #CONGELANDO UM CONJUNTO IMPEDINDO SUA MUTABILIDADE
+
+print('\n-----------------------------------------\n')
+
+texto = 'Meu nome é Robert e eu sou um ser humano muito divertido que trabalha todo dia porque sou muito trabalhador'
+print(set(texto.split()))   #COLOCANDO EM UM CONJUNTO CADA PALAVRA INDIVIDUAL QUE EXISTE NA STRING
