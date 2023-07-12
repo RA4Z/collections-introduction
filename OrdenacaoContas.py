613be35aef6e94b4c8676389207cbeef46fedc7a
RA4Z <rraz639@gmail.com>
2023-07-12 11:05:11 -0300

    Adicionando macete para comparação completa

--- OrdenacaoContas.py
+++ OrdenacaoContas.py
@@ -1,3 +1,7 @@
+from operator import attrgetter
+from functools import total_ordering    #ADICIONANDO FUNCIONALIDADE DE VERIFICAR SE É MENOR OU IGUAL
+
+@total_ordering
 class ContaSalario:
     def __init__(self, codigo):
         self._codigo = codigo
@@ -10,25 +14,36 @@ class ContaSalario:
         return '[>>Codigo {} Saldo {}<<]'.format(self._codigo, self._saldo)
     
     def __lt__(self, outro):
-        return self._saldo < outro._saldo
+        if self._saldo != outro._saldo:
+            return self._saldo < outro._saldo
+        return self._codigo < outro._codigo # ADICIONANDO SEGUNDA FORMA DE COMPARAÇÃO ENTRE CONTAS
 
+    #COMPARAÇÃO PARA VER SE OS DOIS OBJETOS SÃO IGUAIS
     def __eq__(self, outro):
         if type(outro) != ContaSalario:
             return False
         return self._codigo == outro._codigo and self._saldo == outro._saldo
     
-conta_raz = ContaSalario(17)
+conta_raz = ContaSalario(1700)
 conta_raz.deposita(500)
 
 conta_karol = ContaSalario(3)
 conta_karol.deposita(1000)
 
-contas = [conta_raz, conta_karol]
+conta_outro = ContaSalario(133)
+conta_outro.deposita(500)
+
+contas = [conta_raz, conta_karol, conta_outro]
 
 for conta in contas:
     print(conta)
 
-print(conta_karol > conta_raz)
+print(conta_karol <= conta_raz)
 
-for conta in sorted(contas, reverse=True):
+# USANDO MÉTODO attrgetter PARA TER MAIS UMA FORMA DE COMPARAÇÃO
+#for conta in sorted(contas, key=attrgetter('_saldo', '_codigo')):
+#    print(conta)
+
+print('------------------------------------')
+for conta in sorted(contas):
     print(conta)
